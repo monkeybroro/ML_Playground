dd65e1266c76e88eb2d472c46569bcd3bfd6010f
yuan <yuan.liu_3@philips.com>
2018-01-03 12:49:16 +0800

    First check in.

---
+++ chepai.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+
+price=np.genfromtxt("chepai.csv", delimiter=",")
+x = price[:,0:2]
+y = price[:,2]
+
+#Simple linear regression
+# print("Using linear regression..")
+# reg = linear_model.LinearRegression()
+# reg.fit(x, y)
+
+# prediction = np.dot(x, reg.coef_)
+# print(reg.coef_)
+# print(y-prediction)
+
+#Polynomial regression degree 2
+print("Using 3 degree Polynomial regression..")
+poly = PolynomialFeatures(degree=3)
+X_ = poly.fit_transform(x)
+
+clf = linear_model.LinearRegression()
+clf.fit(X_, y)
+print(y-clf.predict(X_))
